2a1c0506ac72b51c0b03028f70cecc38a01d4111
Tidyco <daniel.limb@tidyco.co.uk>
2026-03-09 22:36:39 +0000

    Update helpers.js

--- utils/js/helpers.js
+++ utils/js/helpers.js
@@ -46,3 +46,11 @@ function sortedPfd(pfd) {
 function calcRPN(r) {
   return (r.sev || 1) * (r.occ || 1) * (r.det || 1);
 }
+
+
+function getWeekNumber(d) {
+  d = new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate()));
+  d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay() || 7));
+  var yearStart = new Date(Date.UTC(d.getUTCFullYear(), 0, 1));
+  return Math.ceil((((d - yearStart) / 86400000) + 1) / 7);
+}
